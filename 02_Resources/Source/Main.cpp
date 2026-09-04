e90f99f68c84a5016a1b87b1b1f58f88b94b6ade
Yngu196 <209092336+Yngu196@users.noreply.github.com>
2026-09-04 23:27:13 +0800

    更新02_Resources的函数调用

--- 02_Resources/Source/Main.cpp
+++ 02_Resources/Source/Main.cpp
@@ -17,7 +17,7 @@ namespace Textures
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "Resources");
+	sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "Resources");
 	window.setFramerateLimit(20);
 
 	// Try to load resources
@@ -36,15 +36,16 @@ int main()
 	// Access resources
 	sf::Sprite landscape(textures.get(Textures::Landscape));
 	sf::Sprite airplane(textures.get(Textures::Airplane));
-	airplane.setPosition(200.f, 200.f);
+	airplane.setPosition(sf::Vector2f(200.f, 200.f));
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		while (const auto event=window.pollEvent())
 		{
-			if (event.type == sf::Event::KeyPressed || event.type == sf::Event::Closed)
-				return 0;
+			if (event->is<sf::Event::Closed>() || event->getIf<sf::Event::KeyPressed>())
+			{
+				window.close();
+			}
 		}
 
 		window.clear();
